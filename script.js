775fac0d9b1ddcd4c2c139925f934d05aeef96bd
csmueed <csmueed@gmail.com>
2025-07-30 10:27:08 +0500

    added features of filters, tweak design, and now it is a fully function todoapp but without storage

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const input = document.querySelector("#input");
 const addButton = document.querySelector("#add-button");
 const lists = document.querySelector(".lists");
+const allTasks = document.querySelector("#all-tasks");
+const completedTasks = document.querySelector("#completed-tasks");
+const incompleteTasks = document.querySelector("#Incomplete-tasks");
 
 function addTask() {
   addButton.addEventListener("click", function (e) {
+    if (input.value.trim() === "") return;
     const listItem = document.createElement("li");
     const deleteButton = document.createElement("button");
     deleteButton.className = "deleteButton";
     deleteButton.innerHTML = "Delete";
     const newTask = input.value;
-    listItem.innerHTML = newTask;
+    listItem.textContent = newTask;
     listItem.appendChild(deleteButton);
+    listItem.classList.add("todo");
     lists.appendChild(listItem);
     deleteButton.addEventListener("click", function (e) {
       listItem.remove();
@@ -20,12 +25,39 @@ function addTask() {
         listItem.classList.toggle("completed-todotask");
       }
     });
-
     clearInputField();
   });
 }
+
 addTask();
+filteration();
 
 function clearInputField() {
   input.value = "";
 }
+
+function filteration () {
+  allTasks.addEventListener("click", function (e) {
+  document.querySelectorAll(".todo").forEach((tasks) => {
+    tasks.style.display = "list-item";
+  });
+});
+completedTasks.addEventListener("click", function (e) {
+  document.querySelectorAll(".todo").forEach((tasks) => {
+    if (tasks.classList.contains("completed-todotask")) {
+      tasks.style.display = "list-item";
+    } else {
+      tasks.style.display = "none";
+    }
+  });
+});
+incompleteTasks.addEventListener("click", function (e) {
+  document.querySelectorAll(".todo").forEach((tasks) => {
+    if (!tasks.classList.contains("completed-todotask")) {
+      tasks.style.display = "list-item";
+    } else {
+      tasks.style.display = "none";
+    }
+  });
+});
+};
